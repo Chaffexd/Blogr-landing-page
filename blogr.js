1e2b87738b4ce5064abd576136b790939b9a8ad3
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-09-08 10:03:11 +0800

    Commit finished proj

--- blogr.js
+++ blogr.js
@@ -1,13 +1,31 @@
 const dropdownButtons = document.querySelectorAll(".dropdownButton");
 const dropdownNav = document.querySelectorAll(".dropdown");
 const navBar = document.querySelector("#navBar");
+const mobileNav = document.getElementById("mobileNav");
+const mobileDropDown = document.getElementById("mobileDropDown");
+const mobileDropDownButtons = document.querySelectorAll(".mobDropdownButton");
+const mobileDropDownNav = document.querySelectorAll(".mobdropdown")
 
 function toggleDropdown(ind) {
     dropdownNav[ind].classList.toggle("show")
 }
 
+function mobileToggleDropdown(idx) {
+    mobileDropDownNav[idx].classList.toggle("show")
+}
+
 dropdownButtons.forEach((btn, index) => {
     btn.addEventListener("click", () => {
         toggleDropdown(index)
+    })
+})
+
+mobileNav.addEventListener("click", () => {
+    mobileDropDown.classList.toggle("show")
+})
+
+mobileDropDownButtons.forEach((btn, idx) => {
+    btn.addEventListener("click", () => {
+        mobileToggleDropdown(idx)
     })
 })
